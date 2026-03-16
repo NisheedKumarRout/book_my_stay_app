3e40fc72a7fe76bb920c61e64f85de338a6a23f0
Nisheed Kumar Rout <nisheedkumarrout@gmail.com>
2026-03-16 14:38:46 +0530

    Initial commit: UC1 BookMyStayApp

---
+++ src/bookMyStayApp.java
@@ -0,0 +1,11 @@
+void main() {
+    System.out.println("======================================");
+    System.out.println(" Welcome to Book My Stay Application ");
+    System.out.println(" Hotel Booking Management System v1.0 ");
+    System.out.println("======================================");
+
+    System.out.println("Application started successfully.");
+    System.out.println("Thank you for using Book My Stay!");
+
+    System.out.println("Application shutting down...");
+}
